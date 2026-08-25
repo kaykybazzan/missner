454226c1100b524efe44f7033a0633aedba90fe8
kaykybazzan <kaykybazzan@gmail.com>
2026-08-24 22:39:52 -0300

    Esconder a barra de rolagem no Chrome

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,5 +18,5 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return <html lang="pt-BR" className="bg-background"><head>
         <Script src="https://unpkg.com/split-type" strategy="beforeInteractive" />
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" strategy="beforeInteractive" />
-      </head><body className="antialiased no-scrollbar">{children}{process.env.NODE_ENV === 'production' && <Analytics />}</body></html>
+      </head><body className="antialiased">{children}{process.env.NODE_ENV === 'production' && <Analytics />}</body></html>
 }
